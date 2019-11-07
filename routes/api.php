8eff7e1037305ead32ed1fcffeff49f3a0394936
DanielSaenz2001 <daniel.saenz@upeu.edu.pe>
2019-11-07 18:23:33 -0500

    cors

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
-header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Authorization,Origin, Content-Type, X-Auth-Token, X-XSRF-TOKEN');
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+
 
 Route::group([
     
